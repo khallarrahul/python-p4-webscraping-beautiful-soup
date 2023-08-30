34d61a3460fe7934d6a49bc86c3fe3da64bd70e6
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-08-30 16:46:58 -0700

    adds

--- lib/scraper.py
+++ lib/scraper.py
@@ -2,5 +2,12 @@ from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
-headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+headers = {"user-agent": "my-app/0.0.1"}
+html = requests.get("https://flatironschool.com/career-services/", headers=headers)
+
+doc = BeautifulSoup(html.text, "html.parser")
+courses = doc.select(".heading-25-extrabold color-black")
+
+# Using list comprehension to apply .strip() to each element
+for course in courses:
+    print(course.content)
